da0817fcc1b3246e64b77702f3e5b1334af4e823
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-03-23 19:03:42 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.2%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            
-            if(nums[i]==target){
-                cout<<" "<<i;
-                return i;
+        int st =0;int end=n-1;
+        
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(target>nums[mid]){
+                st =mid+1;
+            }
+            if(target<nums[mid]){
+                end=mid-1;
+            }
+            if(target == nums[mid]){
+                return mid;
             }
         }
-             return -1;
-        }
+        return -1;
+    }
 };
