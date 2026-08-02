5e2a29ae42b2ea923ad8c18c830ca0bd1efe2c89
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-08-02 21:08:36 +0530

    Time: 7 ms (13.49%), Space: 8.7 MB (37.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-
-        int reversed_half = 0;
-        while (x > reversed_half) {
-            reversed_half = reversed_half * 10 + x % 10;
-            x /= 10;
+        if (x< 0){return false;}
+        int k ;
+        long long sum =0  ;
+        int temp = x;
+        while ( temp > 0 ){
+            k = temp % 10 ;
+            sum = (sum *10) + k;
+            temp = temp /10;
         }
-     return x == reversed_half || x == reversed_half / 10;
+        if ( sum  == x ){
+            return true;
+        }else{
+            return false;
+        }
+      
     }
 };
