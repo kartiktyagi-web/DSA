f1222ef7c70b67ce776bc53f985fdf48ce8962da
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-08-03 21:54:10 +0530

    Time: 0 ms (100%), Space: 22.6 MB (81.42%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,15 +3,15 @@ public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.empty()) return 0;
 
-        int insertPos = 1; // Points to where the next unique element goes
+        int k = 1;
 
         for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[i - 1]) { // Found a new unique element
-                nums[insertPos] = nums[i]; // Place it at insertPos
-                insertPos++;
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
-        return insertPos; // Number of unique elements
+        return k;
     }
 };
