bdc2513fcc8a00f659250d413bdab51f4625f874
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-08-13 22:11:24 +0530

    Time: 0 ms (100%), Space: 97.3 MB (88.02%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {\
-    int bestbuy =prices[0];
-  
-    int maxprofit=0;
-    for(int i=1;i<prices.size();i++){
-        if(prices[i]>bestbuy){
-            maxprofit =max(maxprofit,prices[i]-bestbuy);
+    int maxProfit(vector<int>& prices) {
+        int bestbuy = prices[0]; // this is the first element of the  given array which further is used to get compare with others
+        int maxprofit = 0 ;
 
+        for(int i = 1;i<prices.size();i++){
+            if(prices[i]>bestbuy){
+                maxprofit = max(maxprofit , prices[i]-bestbuy );
+            }
+            bestbuy = min(bestbuy, prices[i]);
         }
-        bestbuy = min(bestbuy , prices[i]);
-    }
         return maxprofit;
     }
 };
