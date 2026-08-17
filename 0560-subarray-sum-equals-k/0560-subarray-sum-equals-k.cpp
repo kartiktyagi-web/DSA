074079585b57660c1f2c9fd37810bb9b64728a61
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-08-17 11:06:19 +0530

    Time: 2804 ms (5.02%), Space: 34.3 MB (94.27%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count = 0;
-        
-        // Outer loop for the starting point of the subarray
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            // Inner loop for the ending point of the subarray
-            for (int j = i; j < n; j++) {
+        int count = 0 ;
+        for(int i =0 ;i< nums.size();i++){
+            int sum =0 ;
+            for(int j = i ;j < nums.size();j++){
                 sum += nums[j];
-                if (sum == k) {
-                    count++;
+                if(sum   == k){
+                    count ++;
                 }
             }
-        } 
-        
-        return count;
+        }
+        return count ;
     }
 };
