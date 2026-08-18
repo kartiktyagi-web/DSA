68d4d0945eaa451724b0b5eb4a4e6615574abb5a
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-08-18 21:18:22 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int nonzero = 0;
+        int nonzero =0 ;
         int n = nums.size();
-        for(int i = 0; i < n; i++) { 
-             if (nums[i] != 0) {
-                    swap(nums[nonzero], nums[i]);
-                    nonzero++;
-             }
+        for(int i =0 ;i< n ;i++){
+            if(nums[i]!=0){
+                swap(nums[nonzero],nums[i]);
+                nonzero++;
+            }
         }
-    } 
-}; 
+    }
+};
