a2d4d5cd2e9f409cab50a4b266c0b10653944f7e
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-08-26 22:23:45 +0530

    Time: 0 ms (100%), Space: 27.2 MB (81.11%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
-        int st = 0 ;
-        int end = n-1;
-        while(st<end){
-            char temp = s[st];
-            s[st] = s[end];
-            s[end] =temp;
-            st++;
-            end--;
-        }
+        reverse(s.begin(),s.end());
     }
 };
