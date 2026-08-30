8a8326019831513bab0472560987f19626d02935
KARTIK TYAGI <kartiktyagi326@gmail.com>
2026-08-30 22:14:44 +0530

    Time: 0 ms (100%), Space: 26.5 MB (27.56%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        int duplicate = -1, missing = -1;
+        
+        vector<int> count(n + 1, 0);
+        for (int num : nums) {
+            count[num]++;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == 2) {
+                duplicate = i;
+            } else if (count[i] == 0) {
+                missing = i;
+            }
+        }
+        
+        return {duplicate, missing};
+    }
+};
